64415f80442a683a2f258a90d969667a2e8fb8e1
Anders Bakken <agbakken@gmail.com>
2014-07-08 00:06:18 -0700

    Remove printfs

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,13 @@ int main(int argc, char** argv)
 
         WindowManager manager;
         if (!manager.init(argc, argv)) {
-            printf("[%s:%d]: if (!manager.init(argc, argv))\n", __FILE__, __LINE__); fflush(stdout);
             return 1;
         }
         loop->exec();
-        printf("[%s:%d]: loop->exec();\n", __FILE__, __LINE__); fflush(stdout);
         cleanupLogging();
 
         if (!manager.shouldRestart()) {
-            printf("[%s:%d]: if (!manager.shouldRestart()) {\n", __FILE__, __LINE__); fflush(stdout);
             break;
-        } else {
-            printf("[%s:%d]: } else {\n", __FILE__, __LINE__); fflush(stdout);
         }
     }
     return 0;
