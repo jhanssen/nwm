437a3f2b00d37ebfc603df5620f27e981453c991
Anders Bakken <agbakken@gmail.com>
2014-07-16 12:42:47 -0700

    ws.

--- src/nwm-launcher.sh
+++ src/nwm-launcher.sh
@@ -4,8 +4,8 @@ while true; do
     nwm $@
     [ $? -ne 100 ] && break
     git sync
-    if [ -f build.ninja ]; then 
-        ninja 
+    if [ -f build.ninja ]; then
+        ninja
     else
         make
     fi
